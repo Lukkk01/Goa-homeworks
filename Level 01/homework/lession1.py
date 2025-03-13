50ef0e6b2e1c58ccc1220a962eb8c9eaa81f23c9
Lukkk01 <lukalefs10@gmail.com>
2025-03-13 23:08:29 +0400

    commit

---
+++ Level 01/homework/lession1.py
@@ -0,0 +1,46 @@
+from turtle import*
+
+#we want to draw a house
+
+#step one we want square
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(180)
+forward(200)
+right(45)
+forward(145)
+
+right(90)
+forward(145)
+
+left(180)
+forward(145)
+
+left(90)
+forward(145)
+
+left(45)
+forward(200)
+
+left(90)
+forward(90)
+
+left(90)
+forward(50)
+
+right(90)
+forward(10)
+
+right(90)
+forward(50)
+
+exitonclick()
+
---
+++ Level 04/classwork/task.py
@@ -0,0 +1,30 @@
+# 1)
+num1 = 50
+
+num2 = 14
+
+num3 = num1 + num2
+print(num3)
+
+# 2)
+#Print(hello)
+#print(bye)
+#print("giorgi"+14)
+#num 2 =500
+#name=luka
+
+print("hello")
+print("bye")
+print("giorgi"+"14")
+num2 = 500
+name="luka"
+
+# 3)
+#debuging-არის კოდის შეცდომის პოვნა და გასწორება
+
+
+
+
+
+
+
