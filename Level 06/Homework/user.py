450c64506ae91d6d2bd21ed90b9f667581339d43
Lukkk01 <lukalefs10@gmail.com>
2025-07-21 22:51:15 +0400

    second commit

---
+++ Level 06/Homework/user.py
@@ -0,0 +1,23 @@
+# get user name
+first_name = ("Enter your first name: ")
+last_name = ("Enter your last name: ")
+age = ("Enter your age: ")
+city = ("Enter your city: ")
+
+# Print the information as a sentence
+print("Hello, my name is {first_name} {last_name}. I am {age} years old and I live in {city}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
